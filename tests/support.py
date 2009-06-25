5b09c8f0df1ae2cb5ce890935836f2311441b9ae
Jerry Seutter <jseutter@gmail.com>
2009-06-24 23:00:56 -0600

    Fixture files load more intelligently

---
+++ tests/support.py
@@ -0,0 +1,8 @@
+import os
+
+def open_file(filename):
+    ''' Load a file from the fixtures directory. '''
+    path = 'fixtures/' + filename
+    if ('tests' in os.listdir('.')):
+        path = 'tests/' + path
+    return file(path)
