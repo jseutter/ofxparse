f2fbcf6807c9fca483d2ab2dd9c22d12f6282f2f
Jerry Seutter <jseutter@gmail.com>
2013-02-13 01:00:37 -0700

    PEP 8 cleanup

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ VERSION = re.search(r"__version__ = '(.*?)'",
 
 setup(name='ofxparse',
       version=VERSION,
-      description="Tools for working with the OFX (Open Financial Exchange) file format",
+      description="Tools for working with the OFX (Open Financial Exchange)"
+                  " file format",
       long_description=open("./README", "r").read(),
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -37,6 +38,6 @@ setup(name='ofxparse',
       ],
       entry_points="""
       """,
-      use_2to3 = True,
-      test_suite = 'tests',
+      use_2to3=True,
+      test_suite='tests',
       )
