cef6d7996ebc5f2713e0baf788f14ba6bb546276
Brett Trotter <brett.trotter@webfilings.com>
2014-04-28 13:16:53 -0500

    Refactor line printing

--- ofxparse/ofxprinter.py
+++ ofxparse/ofxprinter.py
@@ -1,19 +1,40 @@
 class OfxPrinter():
     ofx = None
+    out_filename = None
+    out_handle = None
+    term = "\r\n"
 
-    def __init__(self, ofx):
+    def __init__(self, ofx, filename, term="\r\n"):
         self.ofx = ofx
+        self.out_filename = filename
+        self.term = term
 
-    def writeHeaders(self, ofh):
+    def writeLine(self, data, tabs=0, term=None):
+        if term is None:
+            term = self.term
+
+        tabbing = (tabs * "\t") if (tabs > 0) else ''
+
+        return self.out_handle.write(
+            "{0}{1}{2}".format(
+                tabbing,
+                data,
+                term
+            )
+        )
+
+    def writeHeaders(self):
         for k, v in self.ofx.headers.iteritems():
             if v is None:
-                ofh.write("{0}:NONE\r\n".format(k, v))
+                self.writeLine("{0}:NONE".format(k))
             else:
-                ofh.write("{0}:{1}\r\n".format(k, v))
-        ofh.write("\r\n")
+                self.writeLine("{0}:{1}".format(k, v))
+        self.writeLine("")
 
-    def writeSignOn(self, ofh):
-        ofh.write(self.ofx.signon.__str__())
+    def writeSignOn(self, tabs=0):
+        # signon already has newlines and tabs in it
+        # TODO: reimplement signon printing with tabs
+        self.writeLine(self.ofx.signon.__str__(), term="")
 
     def printDate(self, dt, msec_digs=3):
         strdt = dt.strftime('%Y%m%d%H%M%S')
@@ -24,138 +45,150 @@ class OfxPrinter():
             strdt_msec = strdt_msec[:msec_digs]
         return strdt + '.' + strdt_msec
 
-    def writeTrn(self, ofh, trn):
-        ofh.write("\t\t\t\t\t<STMTTRN>\r\n")
-        ofh.write("\t\t\t\t\t\t<TRNTYPE>{}\r\n".format(
-            trn.type.upper()
-        ))
-        ofh.write("\t\t\t\t\t\t<DTPOSTED>{}\r\n".format(
+    def writeTrn(self, trn, tabs=5):
+        self.writeLine("<STMTTRN>", tabs=tabs)
+        tabs += 1
+
+        self.writeLine("<TRNTYPE>{}".format(trn.type.upper()), tabs=tabs)
+        self.writeLine("<DTPOSTED>{}".format(
             self.printDate(trn.date)
-        ))
-        ofh.write("\t\t\t\t\t\t<TRNAMT>{0:.2f}\r\n".format(
-            float(trn.amount)
-        ))
+        ), tabs=tabs)
+        self.writeLine("<TRNAMT>{0:.2f}".format(float(trn.amount)), tabs=tabs)
 
-        ofh.write("\t\t\t\t\t\t<FITID>{}\r\n".format(
-            trn.id
-        ))
+        self.writeLine("<FITID>{}".format(trn.id), tabs=tabs)
 
-        if trn.checknum:
-            ofh.write("\t\t\t\t\t\t<CHECKNUM>{}\r\n".format(
+        if len(str(trn.checknum)) > 0:
+            self.writeLine("<CHECKNUM>{}".format(
                 trn.checknum
-            ))
+            ), tabs=tabs)
 
-        ofh.write("\t\t\t\t\t\t<NAME>{}\r\n".format(
-            trn.payee
-        ))
+        self.writeLine("<NAME>{}".format(trn.payee), tabs=tabs)
 
-        if trn.memo.strip():
-            ofh.write("\t\t\t\t\t\t<MEMO>{}\r\n".format(
-                trn.memo
-            ))
+        if len(trn.memo.strip()) > 0:
+            self.writeLine("<MEMO>{}".format(trn.memo), tabs=tabs)
 
-        ofh.write("\t\t\t\t\t</STMTTRN>\r\n")
+        tabs -= 1
+        self.writeLine("</STMTTRN>", tabs=tabs)
 
-    def writeLedgerBal(self, ofh, statement):
+    def writeLedgerBal(self, statement, tabs=4):
         bal = getattr(statement, 'balance')
         baldt = getattr(statement, 'balance_date')
 
         if bal and baldt:
-            ofh.write("\t\t\t\t<LEDGERBAL>\r\n")
-            ofh.write("\t\t\t\t\t<BALAMT>{0:.2f}\r\n".format(
-                float(bal)
-            ))
-            ofh.write("\t\t\t\t\t<DTASOF>{0}\r\n".format(
+            self.writeLine("<LEDGERBAL>", tabs=tabs)
+            self.writeLine("<BALAMT>{0:.2f}".format(float(bal)), tabs=tabs+1)
+            self.writeLine("<DTASOF>{0}".format(
                 self.printDate(baldt)
-            ))
-            ofh.write("\t\t\t\t</LEDGERBAL>\r\n")
+            ), tabs=tabs+1)
+            self.writeLine("</LEDGERBAL>", tabs=tabs)
 
-    def writeAvailBal(self, ofh, statement):
+    def writeAvailBal(self, statement, tabs=4):
         bal = getattr(statement, 'available_balance')
         baldt = getattr(statement, 'available_balance_date')
 
         if bal and baldt:
-            ofh.write("\t\t\t\t<AVAILBAL>\r\n")
-            ofh.write("\t\t\t\t\t<BALAMT>{0:.2f}\r\n".format(
-                float(bal)
-            ))
-            ofh.write("\t\t\t\t\t<DTASOF>{0}\r\n".format(
+            self.writeLine("<AVAILBAL>", tabs=tabs)
+            self.writeLine("<BALAMT>{0:.2f}".format(float(bal)), tabs=tabs+1)
+            self.writeLine("<DTASOF>{0}".format(
                 self.printDate(baldt)
-            ))
-            ofh.write("\t\t\t\t</AVAILBAL>\r\n")
+            ), tabs=tabs+1)
+            self.writeLine("</AVAILBAL>", tabs=tabs)
 
-    def writeStmTrs(self, ofh):
+    def writeStmTrs(self, tabs=3):
         for acct in self.ofx.accounts:
-            ofh.write("\t\t\t<STMTRS>\r\n")
+            self.writeLine("<STMTRS>", tabs=tabs)
+            tabs += 1
 
             if acct.curdef:
-                ofh.write("\t\t\t\t<CURDEF>{0}\r\n".format(
+                self.writeLine("<CURDEF>{0}".format(
                     acct.curdef
-                ))
+                ), tabs=tabs)
 
             if acct.routing_number or acct.account_id or acct.account_type:
-                ofh.write("\t\t\t\t<BANKACCTFROM>\r\n")
+                self.writeLine("<BANKACCTFROM>", tabs=tabs)
                 if acct.routing_number:
-                    ofh.write("\t\t\t\t\t<BANKID>{0}\r\n".format(
+                    self.writeLine("<BANKID>{0}".format(
                         acct.routing_number
-                    ))
+                    ), tabs=tabs+1)
                 if acct.account_id:
-                    ofh.write("\t\t\t\t\t<ACCTID>{0}\r\n".format(
+                    self.writeLine("<ACCTID>{0}".format(
                         acct.account_id
-                    ))
+                    ), tabs=tabs+1)
                 if acct.account_type:
-                    ofh.write("\t\t\t\t\t<ACCTTYPE>{0}\r\n".format(
+                    self.writeLine("<ACCTTYPE>{0}".format(
                         acct.account_type
-                    ))
-                ofh.write("\t\t\t\t</BANKACCTFROM>\r\n")
+                    ), tabs=tabs+1)
+                self.writeLine("</BANKACCTFROM>", tabs=tabs)
 
-            ofh.write("\t\t\t\t<BANKTRANLIST>\r\n")
-            ofh.write("\t\t\t\t\t<DTSTART>{0}\r\n".format(
+            self.writeLine("<BANKTRANLIST>", tabs=tabs)
+            tabs += 1
+            self.writeLine("<DTSTART>{0}".format(
                 self.printDate(acct.statement.start_date)
-            ))
-            ofh.write("\t\t\t\t\t<DTEND>{0}\r\n".format(
+            ), tabs=tabs)
+            self.writeLine("<DTEND>{0}".format(
                 self.printDate(acct.statement.end_date)
-            ))
+            ), tabs=tabs)
 
             for trn in acct.statement.transactions:
-                self.writeTrn(ofh, trn)
+                self.writeTrn(trn, tabs=tabs)
 
-            ofh.write("\t\t\t\t</BANKTRANLIST>\r\n")
+            tabs -= 1
 
-            self.writeLedgerBal(ofh, acct.statement)
-            self.writeAvailBal(ofh, acct.statement)
+            self.writeLine("</BANKTRANLIST>", tabs=tabs)
 
-            ofh.write("\t\t\t</STMTRS>\r\n")
+            self.writeLedgerBal(acct.statement, tabs=tabs)
+            self.writeAvailBal(acct.statement, tabs=tabs)
 
-    def writeBankMsgsRsv1(self, ofh):
-        ofh.write("\t<BANKMSGSRSV1>\r\n")
-        ofh.write("\t\t<STMTTRNRS>\r\n")
+            tabs -= 1
+
+            self.writeLine("</STMTRS>", tabs=tabs)
+
+    def writeBankMsgsRsv1(self, tabs=1):
+        self.writeLine("<BANKMSGSRSV1>", tabs=tabs)
+        tabs += 1
+        self.writeLine("<STMTTRNRS>", tabs=tabs)
+        tabs += 1
         if self.ofx.trnuid is not None:
-            ofh.write("\t\t\t<TRNUID>{0}\r\n".format(
+            self.writeLine("<TRNUID>{0}".format(
                 self.ofx.trnuid
-            ))
+            ), tabs=tabs)
         if self.ofx.status:
-            ofh.write("\t\t\t<STATUS>\r\n")
-            ofh.write("\t\t\t\t<CODE>{0}\r\n".format(
+            self.writeLine("<STATUS>", tabs=tabs)
+            self.writeLine("<CODE>{0}".format(
                 self.ofx.status['code']
-            ))
-            ofh.write("\t\t\t\t<SEVERITY>{0}\r\n".format(
+            ), tabs=tabs+1)
+            self.writeLine("<SEVERITY>{0}".format(
                 self.ofx.status['severity']
-            ))
-            ofh.write("\t\t\t</STATUS>\r\n")
-        self.writeStmTrs(ofh)
-        ofh.write("\t\t</STMTTRNRS>\r\n")
-        ofh.write("\t</BANKMSGSRSV1>\r\n")
+            ), tabs=tabs+1)
+            self.writeLine("</STATUS>", tabs=tabs)
+        self.writeStmTrs(tabs=tabs)
+        tabs -= 1
+        self.writeLine("</STMTTRNRS>", tabs=tabs)
+        tabs -= 1
+        self.writeLine("</BANKMSGSRSV1>", tabs=tabs)
 
-    def writeOfx(self, ofh):
-        ofh.write("<OFX>\r\n")
-        self.writeSignOn(ofh)
-        self.writeBankMsgsRsv1(ofh)
-        ofh.write("</OFX>\r\n")
+    def writeOfx(self, tabs=0):
+        self.writeLine("<OFX>", tabs=tabs)
+        tabs += 1
+        self.writeSignOn(tabs=tabs)
+        self.writeBankMsgsRsv1(tabs=tabs)
+        tabs -= 1
+        # No newline at end of file
+        self.writeLine("</OFX>", tabs=tabs, term="")
 
-    def write(self, outfilename):
-        outfile = open(outfilename, 'wb')
-        self.writeHeaders(outfile)
-        self.writeOfx(outfile)
-        outfile.flush()
-        outfile.close()
+    def write(self, filename=None, tabs=0):
+        if self.out_handle:
+            raise Exception("Already writing file")
+
+        if filename is None:
+            filename = self.out_filename
+
+        self.out_handle = open(filename, 'wb')
+
+        self.writeHeaders()
+
+        self.writeOfx(tabs=tabs)
+
+        self.out_handle.flush()
+        self.out_handle.close()
+        self.out_handle = None
