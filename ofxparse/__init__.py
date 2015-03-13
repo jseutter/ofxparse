0e183cbd1baf247c70e5f44ced24a873f2e50458
Rehan Khwaja <rehan@khwaja.name>
2015-03-12 21:38:03 -0700

    Add OfxParserException to __init__.py so that we can catch it in client code

--- ofxparse/__init__.py
+++ ofxparse/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 
-from .ofxparse import OfxParser, AccountType, Account, Statement, Transaction
+from .ofxparse import OfxParser, OfxParserException, AccountType, Account, Statement, Transaction
 
 __version__ = '0.14'
