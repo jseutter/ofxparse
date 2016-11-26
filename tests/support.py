c3f94790e8d4d7bca68eb86d1172c9f69f1c070c
tonnydourado <tonnydourado@gmail.com>
2016-11-26 01:12:45 -0200

    Support opening files as text streams on tests

--- tests/support.py
+++ tests/support.py
@@ -1,9 +1,9 @@
 import os
 
 
-def open_file(filename):
+def open_file(filename, mode='rb'):
     ''' Load a file from the fixtures directory. '''
     path = 'fixtures/' + filename
     if ('tests' in os.listdir('.')):
         path = 'tests/' + path
-    return open(path, mode='rb')
+    return open(path, mode=mode)
