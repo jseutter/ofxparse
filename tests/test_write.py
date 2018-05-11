ec5930d50b83f74380beca7d6c45219142c21178
Rehan Khwaja <rehan@khwaja.name>
2018-05-10 21:20:37 -0700

    Support writing to file object with OfxPrinter

--- tests/test_write.py
+++ tests/test_write.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from ofxparse import OfxParser, OfxPrinter
 from unittest import TestCase
+from io import StringIO
 from os import close, remove
 from tempfile import mkstemp
 import sys
@@ -22,6 +23,14 @@ class TestOfxWrite(TestCase):
         close(fd)
         printer = OfxPrinter(ofx=ofx, filename=name)
         printer.write(tabs=1)
+
+    def test_using_ofx_printer_with_stringio(self):
+        with open_file('checking.ofx') as f:
+            ofx = OfxParser.parse(f)
+        output_buffer = StringIO()
+        printer = OfxPrinter(ofx=ofx, filename=None)
+        printer.writeToFile(output_buffer, tabs=1)
+        assert output_buffer.getvalue().startswith("OFXHEADER")
 
 if __name__ == "__main__":
     import unittest
