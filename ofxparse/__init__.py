9a2adf9de0fd8273a9be4be760fee8d7bc514513
Jerry Seutter <jerry.seutter@rackspace.com>
2018-08-08 07:54:37 -0500

    Version bump

--- ofxparse/__init__.py
+++ ofxparse/__init__.py
@@ -4,7 +4,7 @@ from .ofxparse import (OfxParser, OfxParserException, AccountType, Account,
                        Statement, Transaction)
 from .ofxprinter import OfxPrinter
 
-__version__ = '0.18'
+__version__ = '0.19'
 __all__ = [
     'OfxParser',
     'OfxParserException',
